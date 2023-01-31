3394e9731a5115c748f748228e891d0d7fb7c50d
Jhon Armando <jhonzeras93@gmail.com>
2023-01-31 02:26:38 -0300

    projeto portifolio basico

---
+++ website/script.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".mySwiper", {
+    cssMode: true,
+    loop: true,
+    navegation: {
+        nextE1: "swiper-button-next",
+        nextE2: "swiper-button-prev",
+    },
+    pagination: {
+        e1: ".swiper-pagination",
+    },
+    mouseshee1: true,
+    keybord: true,
+});
